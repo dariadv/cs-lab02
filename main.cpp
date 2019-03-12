3ef5039e88a0f97babacd2c26375af4d20d9bdcd
Bob (SharovaDV) <bob@example.com>
2019-03-12 16:32:43 +0300

    bob

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ cout << "Enter A and    B: ";
 int a, b;
 cin >> a >> b;
 cout << "A + B = " << a + b << '\n'
- << "A - B = " << a - b << '\n';
+ << "A - B = " << a - b << '\n'
+ <<"A*B="<<a*b<<'\n';
 return 0;
 }
