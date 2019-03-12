d03cb47a1ab0b64b57097b94cbc86d00a59e6d71
Alice (SharovaDV) <alice@example.com>
2019-03-12 17:03:36 +0300

    nov

--- main.cpp
+++ main.cpp
@@ -5,11 +5,13 @@ using namespace std;
 int main()
 {
 cout << "Enter A and    B: ";
-int a, b;
+int a, b,max;
 cin >> a >> b;
 cout << "A + B = " << a + b << '\n'
  << "A - B = " << a - b << '\n'
  <<"A*B="<<a*b<<'\n';
-  <<"A/B="<<a/b<<'\n';
+  cout <<"A/B="<<a/b<<'\n';
+  cout <" max="< max<'\n';
+
 return 0;
 }
